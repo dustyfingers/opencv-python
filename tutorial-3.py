076e44438f86b5147cdd4b3f83a7f5a1afb41e81
lou <dustyfingers01257@gmail.com>
2021-10-27 08:39:35 -0600

    learned how to draw shapes and display text with opencv

--- tutorial-3.py
+++ tutorial-3.py
@@ -13,12 +13,23 @@ while True:
     # empty image that is the same size as frame 
     image = np.zeros(frame.shape, np.uint8)
 
-    smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    # how to draw
+    image = cv2.line(frame, (0, 0), (width, height), (255, 0, 0), 10)
+    image = cv2.line(image, (0, height), (width, 0), (255, 0, 0), 10)
+    # -1 as the last argument (thickness) fills the shape
+    image = cv2.rectangle(image, (100, 100), (250, 250), (0, 255, 0), 10)
+    image = cv2.circle(image, (300, 300), 60, (0, 0, 255), -1)
 
-    image[:height//2, :width//2] = cv2.rotate(smaller_frame, cv2.ROTATE_180)
-    image[height//2:, :width//2] = smaller_frame
-    image[:height//2, width//2:] = cv2.rotate(smaller_frame, cv2.ROTATE_180)
-    image[height//2:, width//2:] = smaller_frame
+    # how to display text
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    image = cv2.putText(image, 'Lou is awesome!', (20, height-10), font, 1, (0, 0, 0), 3, cv2.LINE_AA)
+
+    # smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+
+    # image[:height//2, :width//2] = cv2.rotate(smaller_frame, cv2.ROTATE_180)
+    # image[height//2:, :width//2] = smaller_frame
+    # image[:height//2, width//2:] = cv2.rotate(smaller_frame, cv2.ROTATE_180)
+    # image[height//2:, width//2:] = smaller_frame
 
     cv2.imshow('Webcam Video', image)
 
