8f25dd7393442756a3b5baee5b0a3c5e1864c084
lou <dustyfingers01257@gmail.com>
2021-10-26 21:01:07 -0600

    load image using opencv!

--- tutorial-1.py
+++ tutorial-1.py
@@ -1,2 +1,13 @@
 import cv2
 
+# load image, pick mode to load image in
+img = cv2.imread('./assets/headshot.jpg', cv2.IMREAD_UNCHANGED)
+# prints a matrix representing the image. neat!
+# print(img)
+
+# first arg is label for window, second is image to display
+cv2.imshow('My Face', img)
+
+# 0 waits an infinite amount of time until you press a key on the kb
+cv2.waitKey(0)
+cv2.destroyAllWindows()
