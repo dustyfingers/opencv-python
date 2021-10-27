df7eacc530aeb6a79e49d34e2bcbda80aa0a43a9
lou <dustyfingers01257@gmail.com>
2021-10-26 23:40:45 -0600

    learn how to edit realtime video data

--- tutorial-3.py
+++ tutorial-3.py
@@ -4,11 +4,23 @@ import cv2
 # VideoCapture can also load video files using the filename
 cap = cv2.VideoCapture(0)
 
-print(cap)
-
 while True:
     ret, frame = cap.read()
-    cv2.imshow('Webcam Video', frame)
+
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    # empty image that is the same size as frame 
+    image = np.zeros(frame.shape, np.uint8)
+
+    smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+
+    image[:height//2, :width//2] = cv2.rotate(smaller_frame, cv2.ROTATE_180)
+    image[height//2:, :width//2] = smaller_frame
+    image[:height//2, width//2:] = cv2.rotate(smaller_frame, cv2.ROTATE_180)
+    image[height//2:, width//2:] = smaller_frame
+
+    cv2.imshow('Webcam Video', image)
 
     if cv2.waitKey(1) == ord('q'):
         break
